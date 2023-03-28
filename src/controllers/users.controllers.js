7a9144f73cf39f3ddf19eb083e106fcd6132880a
Adsuarez <adsuarezq@gmail.com>
2023-03-28 15:57:08 -0500

    change async-await with try-catch to Promises, to somplify code and optimize the catch with next property

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,97 +1,73 @@
 import { pool } from "../db.js";
+import { notFound } from "../helpers/notFound.js";
 
-export const getUsers = async (req, res) => {
-  try {
-    const [rows] = await pool.query("SELECT * FROM user");
-    res.json(rows);
-  } catch (error) {
-    return res.status(500).json({
-      message: error.message,
-    });
-  }
+export const getUsers = (req, res, next) => {
+  pool
+    .query("SELECT * FROM user")
+    .then(([rows]) => res.json(rows))
+    .catch(next);
 };
 
-export const getUser = async (req, res) => {
-  try {
-    const { id } = req.params;
+export const getUser = (req, res, next) => {
+  const { id } = req.params;
 
-    const [[rows]] = await pool.query(`SELECT * FROM user WHERE id = ${id}`);
+  if (/[a-zA-Z]/g.test(id)) return notFound(res, "user");
 
-    if (rows === undefined)
-      return res.status(404).json({ message: "user not found" });
-
-    return res.json(rows);
-  } catch (error) {
-    return res.status(500).json({
-      message: error.message,
-    });
-  }
+  pool
+    .query(`SELECT * FROM user WHERE id = ${id}`)
+    .then(([[rows]]) => {
+      if (rows === undefined) return notFound(res, "user");
+      return res.json(rows);
+    })
+    .catch(next);
 };
 
-export const createUser = async (req, res) => {
-  try {
-    const { userName, myMovies } = req.body;
-    /* Aquí podrían hacerse las validaciones de los datos que llegan */
-    const [rows] = await pool.query(
-      "INSERT INTO user (userName, myMovies) VALUES (?, ?)",
-      [userName, myMovies]
-    );
-    res.status(201).send({
-      id: rows.insertId,
-      userName,
-      myMovies,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: error.message,
-    });
-  }
+export const createUser = (req, res, next) => {
+  const { userName, email, myMovies, schedule } = req.body;
+  pool
+    .query(
+      "INSERT INTO user (userName, email, myMovies, schedule) VALUES (?, ?, ?, ?)",
+      [userName, email, myMovies, schedule]
+    )
+    .then(([rows]) =>
+      res.status(201).json({
+        id: rows.insertId,
+        userName,
+        email,
+        myMovies,
+        schedule,
+      })
+    )
+    .catch(next);
 };
 
-export const updateUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { userName, myMovies } = req.body;
-    const [result] = await pool.query(
-      "UPDATE user SET userName = IFNULL(?, userName), myMovies = IFNULL(?, myMovies) WHERE id = ?",
-      [userName, myMovies, id]
-    );
-
-    if (result.affectedRows === 0)
-      return res.status(404).json({
-        message: "User not found",
-      });
-
-    const [[rows]] = await pool.query("SELECT * FROM user WHERE id = ?", [id]);
-
-    return res.json(rows);
-  } catch (error) {
-    return res.status(500).json({
-      message: error.message,
-    });
-  }
+export const updateUser = (req, res, next) => {
+  const { id } = req.params;
+  const { userName, myMovies, schedule } = req.body;
+  if (/[a-zA-Z]/g.test(id)) return notFound(res, "user");
+  pool
+    .query(
+      "UPDATE user SET userName = IFNULL(?, userName), myMovies = IFNULL(?, myMovies), schedule = IFNULL(?, schedule) WHERE id = ?",
+      [userName, myMovies, schedule, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) return notFound(res, "user");
+      pool
+        .query("SELECT * FROM user WHERE id = ?", [id])
+        .then(([[rows]]) => res.json(rows))
+        .catch(next);
+    })
+    .catch(next);
 };
 
-export const deleteUser = async (req, res) => {
-  try {
-    const [result] = await pool.query(
-      `DELETE FROM user WHERE id = ${req.params.id}`
-    );
-    if (result.affectedRows === 0)
-      return res.status(404).json({
-        message: "User not found",
-      });
-
-    res.sendStatus(204);
-  } catch (error) {
-    return res.status(500).json({
-      message: error.message,
-    });
-  }
+export const deleteUser = async (req, res, next) => {
+  const { id } = req.params;
+  if (/[a-zA-Z]/g.test(id)) return notFound(res, "user");
+  pool
+    .query("DELETE FROM user WHERE id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) return notFound(res, "user");
+      res.sendStatus(204);
+    })
+    .catch(next);
 };
-
-/*
-  This sintax make an error when there are more than one variable to insert
-                                  👇                   👇
-  `UPDATE user SET userName = ${userName} WHERE id = ${id}`
-*/
