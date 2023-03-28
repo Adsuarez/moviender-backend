cd727ae905aaede19b88ebec058faad8adb2cd81
Adsuarez <adsuarezq@gmail.com>
2023-03-28 15:55:11 -0500

    solve problem with json schema and add more dimension to userName and email

--- db_commands.sql
+++ db_commands.sql
@@ -4,8 +4,8 @@ USE moviender_db;
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `userName` VARCHAR(16) DEFAULT NULL,
-  `email` VARCHAR(32) NOT NULL UNIQUE,
+  `userName` VARCHAR(32) DEFAULT NULL,
+  `email` VARCHAR(50) NOT NULL UNIQUE,
   `myMovies` JSON DEFAULT NULL,
   `schedule` JSON DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -16,6 +16,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   SELECT * FROM user;
   
   INSERT INTO user (userName, email, myMovies, schedule) VALUES 
-  ("ads", "ads@hotmail.com", "[{'like': '[45654, 200, 654, 2154]'},{'dislike': '[456545, 212321]'},{'desire': '[]'}]", "['{'id': 12345, 'date': 'mar-31-2023'}', '{'id': 54654, 'date': 'apr-05-2023'}']");
+  ("ads", "ads@hotmail.com", '[{"like": [45654, 200, 654, 2154]},{"dislike": [456545, 212321]},{"desire": []}]', '[{"id": 12345, "date": "mar-31-2023"}, {"id": 54654, "date": "apr-05-2023"}]'); 
+  
+  /*
+  JSON Schema
+  
+  "userName": "loquesea",
+  "email": "loquesea@hotmail.com",
+  "myMovies": '[{"like": [45654, 200, 654, 2154]},{"dislike": [456545, 212321]},{"desire": []}]',
+  "schedule": '[{"id": 12345, "date": "mar-31-2023"}, {"id": 54654, "date": "apr-05-2023"}]'
+  */
   
   -- DROP table user;
