db6e3d161f04d0e1493ef194f878bcf8f05dd2b3
Adsuarez <adsuarezq@gmail.com>
2023-03-29 16:58:16 -0500

    move to config folder for organization

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 import { createPool } from "mysql2/promise";
-import { HOST, USER, PASSWORD, DB_PORT, DATABASE } from "./config.js";
+import { HOST, USER, PASSWORD, DB_PORT, DATABASE } from "./environment.js";
 
 export const pool = createPool({
   host: HOST,
