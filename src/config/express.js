2d3c6e9c4be049271972b0750e2ea560856a20cf
Adsuarez <adsuarezq@gmail.com>
2023-03-29 16:56:45 -0500

    move information from app.js to express.js to allow more organization of concepts

--- src/config/express.js
+++ src/config/express.js
@@ -1,28 +1,28 @@
 import express from "express";
-import indexRoutes from "./routes/index.routes.js";
-import usersRoutes from "./routes/users.routes.js";
 import cors from "cors";
+import indexRoutes from "#Routes/index.routes.js";
+import usersRoutes from "#Routes/users.routes.js";
 
+const expressApp = express();
 console.clear();
-const app = express();
-app.use(cors());
-app.use(express.json()); //Transform datas in JSON to NodeJS can recognize them.
-app.use(express.text());
-app.use((req, res, next) => {
+expressApp.use(cors());
+expressApp.use(express.json()); //Transform datas in JSON to NodeJS can recognize them.
+expressApp.use(express.text());
+expressApp.use((req, res, next) => {
   console.log(req.method);
   console.log(req.path);
   console.log(req.body);
   console.log("------");
   next();
 }); //this is a way to intercept any query
-app.use(indexRoutes);
-app.use("/api", usersRoutes);
-app.use((req, res, next) => {
+expressApp.use(indexRoutes);
+expressApp.use("/api", usersRoutes);
+expressApp.use((req, res, next) => {
   res.status(404).json({
     message: "Endpoint not found",
   });
 }); //this is a middleware function
-app.use((error, req, res, next) => {
+expressApp.use((error, req, res, next) => {
   if (error.name === "CastError")
     return res.status(400).json({
       message: error.message,
@@ -33,4 +33,4 @@ app.use((error, req, res, next) => {
   });
 });
 
-export default app;
+export default expressApp;
