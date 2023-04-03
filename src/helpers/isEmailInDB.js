41bf0fe2c56fb32bf1bef7c2ed9b03872c3797b5
Adsuarez <adsuarezq@gmail.com>
2023-04-03 17:11:08 -0500

    changing object structure, pending solve error with conditional

--- src/helpers/isEmailInDB.js
+++ src/helpers/isEmailInDB.js
@@ -1,12 +1,12 @@
 import { pool } from "#Config/db.js";
 
-export async function isEmailInDB(email) {
+export async function isEmailInDB(emailToSearch) {
   pool
-    .query("SELECT email FROM user WHERE email = ?", [email])
+    .query("SELECT email FROM user WHERE email = ?", [emailToSearch])
     .then(([[rows]]) => {
-      console.log({ rows });
-      if (rows === undefined) return false;
-      return true;
+      console.log(rows);
+      if (typeof rows === "object") return true;
+      return false;
     })
     .catch((error) => {
       console.error(error);
