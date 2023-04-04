4b18bc3ad8f2d36ede736b924634a02beb48798c
Adsuarez <adsuarezq@gmail.com>
2023-04-04 12:39:57 -0500

    add format property to schema and conditions to validate password

--- src/schemas/password.schema.js
+++ src/schemas/password.schema.js
@@ -1,9 +1,13 @@
 export const passwordSchema = {
-  type: "string",
   require: true,
+  type: "string",
+  format: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm,
   unique: false,
 };
 
 export const verifyPasswordSchema = async (password) => {
   if (!password) return false;
+  if (typeof password !== passwordSchema.type) return false;
+  if (!passwordSchema.format.test(password)) return false;
+  return true;
 };
