39e37d49b027fb990625668c5675b75e37f8df54
Adsuarez <adsuarezq@gmail.com>
2023-04-04 12:39:18 -0500

    change order of property according to the order of conditions

--- src/schemas/email.schema.js
+++ src/schemas/email.schema.js
@@ -1,25 +1,17 @@
 import { isEmailInDB } from "#Helpers/isEmailInDB.js";
 
 export const emailSchema = {
-  type: "string",
   require: true,
-  unique: true,
+  type: "string",
   format: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+  unique: true,
 };
 
 export const verifyEmailSchema = async (email) => {
-  console.log("email is not passed");
   if (!email) return false;
-  console.clear();
-  console.log("email is passed");
-  console.log("email is not in correct format");
   if (typeof email !== emailSchema.type) return false;
-  console.clear();
-  console.log("email is a correct format");
-  console.log("email exist yet in database");
-  console.log(await isEmailInDB(email));
-  if (await isEmailInDB(email)) return false;
-  console.clear();
-  console.log("email not exist in database");
+  if (emailSchema.format.test(email) !== true) return false;
+  const isEmailRepeated = await isEmailInDB(email);
+  if (isEmailRepeated) return false;
   return true;
 };
