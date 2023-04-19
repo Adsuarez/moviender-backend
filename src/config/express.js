1bcce8e6c33e0e0d59146c3fe221315e154a0038
Adsuarez <adsuarezq@gmail.com>
2023-04-19 12:52:40 -0500

    change error.name for correct error.code to catch CastError and add status 500 and Bad Field Error

--- src/config/express.js
+++ src/config/express.js
@@ -7,20 +7,23 @@ const expressApp = express();
 console.clear();
 expressApp.use(cors());
 expressApp.use(express.json());
+
+//intercept all queries to debugging information
 expressApp.use((req, res, next) => {
   console.log(req.method);
   console.log(req.path);
   console.log(req.body);
   console.log("------");
   next();
-}); //this is a way to intercept any query
+});
+
 expressApp.use("/api", usersRoutes);
 expressApp.use((req, res, next) => {
   return notFound(res, "endpoint");
 });
 expressApp.use((error, req, res, next) => {
   console.log("from middleware to errors: ", error.code);
-  if (error.name === "CastError")
+  if (error.code === "CastError")
     return res.status(400).json({
       message: error.message,
     });
@@ -30,10 +33,14 @@ expressApp.use((error, req, res, next) => {
       message: error.message,
     });
 
-  if (error.code === "ERR_HTTP_HEADERS_SENT")
-    return res.status(444).json({
+  if (error.code === "ER_BAD_FIELD_ERROR")
+    return res.status(400).json({
       message: error.message,
     });
+
+  return res.status(500).json({
+    message: error.message,
+  });
 });
 
 export default expressApp;
