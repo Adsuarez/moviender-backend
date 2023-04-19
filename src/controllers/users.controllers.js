577464bf6edf63573dd4dc9615089954abd682b4
Adsuarez <adsuarezq@gmail.com>
2023-04-19 12:53:25 -0500

    remove unnecesary return

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -28,7 +28,7 @@ export async function createUser(req, res, next) {
   const id = await generateId();
   const encodePasswords = await encryptPassword(password);
 
-  return pool
+  pool
     .query(
       "INSERT INTO user (id, email, userName, password) VALUES (?, ?, ?, ?)",
       [id, email, userName, encodePasswords]
