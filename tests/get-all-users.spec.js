f5da0f3ce6ac19d7f9a8d0587f5a7b80f4fbc1b8
Adsuarez <adsuarezq@gmail.com>
2023-04-21 17:11:42 -0500

    add constant API_USER in tests files

--- tests/get-all-users.spec.js
+++ tests/get-all-users.spec.js
@@ -1,14 +1,15 @@
 import expressApp from '#Config/express.js'
 import request from 'supertest'
+import { API_USERS } from '#Config/constants.js'
 
-describe('GET /api/users', () => {
+describe(`GET ${API_USERS}`, () => {
   test('should respond with a 200 status code', async () => {
-    const response = await request(expressApp).get('/api/users').send()
+    const response = await request(expressApp).get(API_USERS).send()
     expect(response.statusCode).toBe(200)
   })
 
   test('should respond with an array', async () => {
-    const response = await request(expressApp).get('/api/users').send()
+    const response = await request(expressApp).get(API_USERS).send()
     expect(response.body).toBeInstanceOf(Array)
   })
 })
--- tests/get-user.spec.js
+++ tests/get-user.spec.js
@@ -1,15 +1,16 @@
 import expressApp from '#Config/express.js'
 import request from 'supertest'
+import { API_USERS } from '#Config/constants.js'
 
-describe('GET /api/users/:id', () => {
+describe(`GET ${API_USERS}:id`, () => {
   test('should respond with a 404 status code if given an incorrect id', async () => {
-    const response = await request(expressApp).get('/api/users/123').send()
+    const response = await request(expressApp).get(`${API_USERS}123`).send()
     expect(response.statusCode).toBe(404)
   })
 
   test('should respond with a 200 status code if given a correct id', async () => {
     const response = await request(expressApp)
-      .get('/api/users/1DMfKVrk3_i-eoAqi5lJY')
+      .get(`${API_USERS}1DMfKVrk3_i-eoAqi5lJY`)
       .send()
     expect(response.statusCode).toBe(200)
   })
